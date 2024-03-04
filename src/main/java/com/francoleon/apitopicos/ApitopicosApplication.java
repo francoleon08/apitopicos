358d978ec931604a3e614d1b1cc1f7b091c9ef36
francoleon08 <francoleon284@gmail.com>
2024-03-04 19:21:15 -0300

    init source code

---
+++ src/main/java/com/francoleon/apitopicos/ApitopicosApplication.java
@@ -0,0 +1,13 @@
+package com.francoleon.apitopicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitopicosApplication.class, args);
+	}
+
+}
